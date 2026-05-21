f5bda2bfce6be85650202a5add69ec482e70c0f4
Duy2801 <nguyenducduy544645@gmail.com>
2026-05-21 22:42:25 +0700

    chore: remove constant.ts file containing API constants

--- FrontEnd/gotogetherapp/src/api/constant.ts
+++
@@ -1,5 +0,0 @@
-const ip = '192.168.2.22';
-// const ip = '54.206.71.186';
-export const socket_url = `http://${ip}:3000`
-export const base_url = `http://${ip}:3000/api/v1`;
-export const GOOGLE_CLIENT_ID = '680269445663-d3fu8kab73it2l4i0enhek6ej1ufs3db.apps.googleusercontent.com';
